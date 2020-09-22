a893efc1a156da480667f1929c147e7b10eb6078
Jakub Kopańko <jakub@kopanko.com>
2020-09-22 15:02:34 +0200

    Cleanup, still doesn't work

--- src/WorldState.ts
+++ src/WorldState.ts
@@ -1,41 +1,55 @@
 import { Schema, ArraySchema, type } from '@colyseus/schema';
 
-enum TerrainType {
+export enum ETerrainType {
 	GRASS = 'grass',
 	FOREST = 'forest',
 	ROAD = 'road'
 }
 
-enum BuildingType {
+export enum EBuildingType {
 	HOUSE = 'house',
 	CHIMNEY = 'chimney'
 }
 
 class Tile extends Schema {
-	@type('number')
-	x : number;
-	@type('number')
-	y : number;
-	@type('string')
-	terrain : TerrainType;
+	@type('number') x: number;
+	@type('number') y : number;
+	@type('string') type: string;
+
+	constructor (terrain: ETerrainType, x: number, y?: number) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.type = terrain
+	}	
 	
 }
 
-class Building extends Schema {
-	@type('number')
-	x : number;
-	@type('number')
-	y : number;
-	@type('string')
-	building : BuildingType;
+export class Building extends Schema {
+	@type('number') x: number;
+	@type('number') y: number;
+	@type('string') type: string;
+
+	constructor (type: EBuildingType, x: number, y?: number) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.type = type
+	}	
 }
 
 export class WorldState extends Schema {
+	@type('number')
+	size: number = 40
 	@type([Tile])
 	tiles = new ArraySchema<Tile>();
 
 	@type([Building])
 	buildings = new ArraySchema<Building>();
+
+	constructor () {
+		super();
+	}	
 }
 
 
